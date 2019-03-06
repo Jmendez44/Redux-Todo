5b49c71df310c1177c3703a54d13a92dfde1f03f
jmendez44 <jenuel.mendez@gmail.com>
2019-03-05 19:00:39 -0500

    created store in app

---
+++ todo/src/App.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      todos: []
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        Test
+      </div>
+    );
+  }
+}
+
+export default App;
